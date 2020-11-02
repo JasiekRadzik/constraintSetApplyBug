eb7ed7d1be7c9f30c018f9a3bc5f10460aaaa542
Jan Radzikowski <j.radzikowski@futuremind.com>
2020-11-02 10:53:07 +0100

    Implement welcome activity layout and logic.

--- app/src/main/java/pl/janradzikowski/constraintsetbug/MainActivity.kt
+++ app/src/main/java/pl/janradzikowski/constraintsetbug/MainActivity.kt
@@ -2,10 +2,37 @@ package pl.janradzikowski.constraintsetbug
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintSet
+import pl.janradzikowski.constraintsetbug.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var vb: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+        vb.showContent.setOnClickListener { showContent() }
+        vb.showSplash.setOnClickListener { showSplash() }
+    }
+
+    private fun showSplash() {
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(vb.rootView)
+        constraintSet.setVerticalBias(R.id.logo, 0.5f)
+        constraintSet.setVisibility(R.id.contentGroup, View.GONE)
+        constraintSet.setVisibility(R.id.splashScreen, View.VISIBLE)
+        constraintSet.applyTo(vb.rootView)
+    }
+
+    private fun showContent() {
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(vb.rootView)
+        constraintSet.setVerticalBias(R.id.logo, 0.3f)
+        constraintSet.setVisibility(R.id.contentGroup, View.VISIBLE)
+        constraintSet.setVisibility(R.id.splashScreen, View.GONE)
+        constraintSet.applyTo(vb.rootView)
     }
 }
